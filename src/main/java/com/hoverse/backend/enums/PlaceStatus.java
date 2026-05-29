39a0373a8dd819e88254c371d2d976c8a454e550
Phi Hoang <ntphoang205@gmail.com>
2026-05-29 09:23:20 +0700

    feat(entity): create Place entity with indexes and denormalized fields

---
+++ src/main/java/com/hoverse/backend/enums/PlaceStatus.java
@@ -0,0 +1,13 @@
+package com.hoverse.backend.enums;
+
+/**
+ * Project_TimKiemDiaDiemVuiChoi
+ * Author: Phi Hoàng
+ * Date: 29/05/2026
+ */
+public enum PlaceStatus {
+    PENDING,
+    APPROVED,
+    REJECTED,
+    HIDDEN
+}

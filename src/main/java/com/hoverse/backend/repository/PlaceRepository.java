3165ed079371709aa9e9d2dd8b07165edad2892a
Phi Hoang <ntphoang205@gmail.com>
2026-05-29 10:26:00 +0700

    chore(db): add DatabaseSeeder and basic Repositories to populate mock data

---
+++ src/main/java/com/hoverse/backend/repository/PlaceRepository.java
@@ -0,0 +1,14 @@
+package com.hoverse.backend.repository;
+
+import com.hoverse.backend.entity.Place;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+/**
+ * Project_TimKiemDiaDiemVuiChoi
+ * Author: Phi Hoàng
+ * Date: 29/05/2026
+ */
+@Repository
+public interface PlaceRepository extends JpaRepository<Place,Long> {
+}

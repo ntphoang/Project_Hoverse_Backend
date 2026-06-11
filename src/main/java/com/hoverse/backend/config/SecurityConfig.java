ba979d07749d9901ffd7daca416c44dfcf446f9c
Phi Hoang <ntphoang205@gmail.com>
2026-06-11 15:44:47 +0700

    feat(security): add JWT configuration and utility class for token generation

---
+++ src/main/java/com/hoverse/backend/config/SecurityConfig.java
@@ -0,0 +1,8 @@
+package com.hoverse.backend.config;
+
+/**
+ * Project_Hoverse_Backend
+ * Author: Phi Hoàng
+ * Date: 11/06/2026
+ */public class SecurityConfig {
+}
---
+++ src/main/java/com/hoverse/backend/security/JwtAuthFilter.java
@@ -0,0 +1,8 @@
+package com.hoverse.backend.security;
+
+/**
+ * Project_Hoverse_Backend
+ * Author: Phi Hoàng
+ * Date: 11/06/2026
+ */public class JwtAuthFilter {
+}

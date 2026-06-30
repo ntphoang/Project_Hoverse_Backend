0103efd2cf13742adf850f09963af42bb8b756cf
Phi Hoang <ntphoang205@gmail.com>
2026-06-30 09:59:54 +0700

    feat(place-search-filter): extend JpaSpecificationExcecutor in PlaceRepository

--- src/main/java/com/hoverse/backend/repository/PlaceRepository.java
+++ src/main/java/com/hoverse/backend/repository/PlaceRepository.java
@@ -3,6 +3,7 @@ package com.hoverse.backend.repository;
 import com.hoverse.backend.entity.Place;
 import com.hoverse.backend.enums.PlaceStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
@@ -13,6 +14,6 @@ import java.util.Optional;
  * Date: 29/05/2026
  */
 @Repository
-public interface PlaceRepository extends JpaRepository<Place,Long> {
+public interface PlaceRepository extends JpaRepository<Place,Long>, JpaSpecificationExecutor<Place> {
     Optional<Place> findByIdAndStatus(Long id, PlaceStatus status);
 }

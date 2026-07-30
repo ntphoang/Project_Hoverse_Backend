ec6bda464fec1f9fa041d5785873e63f30ae5b42
Phi Hoang <ntphoang205@gmail.com>
2026-07-30 10:06:31 +0700

    feat/place-favortie: add handle URL "/favorites"

---
+++ src/main/java/com/hoverse/backend/controller/PlaceFavoriteController.java
@@ -0,0 +1,28 @@
+package com.hoverse.backend.controller;
+
+import com.hoverse.backend.service.PlaceFavoriteService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.security.Principal;
+
+/**
+ * Project_Hoverse_Backend
+ * Author: Phi Hoàng
+ * Date: 29/07/2026
+ */
+@RestController
+@RequestMapping("/api/v1/favorites")
+@RequiredArgsConstructor
+public class PlaceFavoriteController {
+    private final PlaceFavoriteService placeFavoriteService;
+
+    @GetMapping()
+    public ResponseEntity<?> getPlaceFavoriteId(Principal principal){
+        String email = principal!=null ? principal.getName() : null;
+        return ResponseEntity.ok(placeFavoriteService.getPlaceFavoriteId(email));
+    }
+}

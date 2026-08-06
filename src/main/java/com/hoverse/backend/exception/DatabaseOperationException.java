8cac59eac07ea8271208712304450bc0dbd643ed
Phi Hoang <ntphoang205@gmail.com>
2026-08-06 22:10:49 +0700

    feat/user-upload-avatar: add global exception DatabaseOperationException

---
+++ src/main/java/com/hoverse/backend/exception/DatabaseOperationException.java
@@ -0,0 +1,10 @@
+package com.hoverse.backend.exception;
+
+/**
+ * Project_Hoverse_Backend
+ * Author: Phi Hoàng
+ * Date: 06/08/2026
+ */
+public class DatabaseOperationException extends RuntimeException{
+    public DatabaseOperationException(String message){super(message);};
+}
